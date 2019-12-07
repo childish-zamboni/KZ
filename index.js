81609b7317b8c0589ff59759c1f84e5711bb7b19
Kailas Moon <kailasmoon@gmail.com>
2019-12-07 18:22:04 -0500

    Installing modules

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/js'));
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/public/index.html');
